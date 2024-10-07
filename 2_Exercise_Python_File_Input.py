4afdab54ec5624de97ac4a8d214f88a18ff12c1e
Jakub Kujawski <jaahcob@gmail.com>
2024-10-07 23:44:51 +0100

    Add files via upload

---
+++ 2_Exercise_Python_File_Input.py
@@ -0,0 +1,8 @@
+yourname = input("What is your name? ")
+print("Your name is: ", yourname)
+age = int(input("What is your age? "))
+print("your age is: ", age)
+year = int(input("What is the current year? "))
+birthage = year - age
+
+print(yourname,", You will turn 100 years old in ", birthage + 100)
